088b9a47a2d7550b2d84fc731128e415a8f1bedf
malapashish <malapashish10@gmail.com>
2021-08-10 17:16:44 +0530

    Code Refactored

--- src/App.js
+++ src/App.js
@@ -1,88 +1,90 @@
-import React , {useState , useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import "./App.css"
+import "./App.css";
 
-import TextField from '@material-ui/core/TextField';  
-import AddIcon from '@material-ui/icons/Add';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles'; 
-import {db} from "./firebaseConfig";
-import firebase from "firebase"; 
+import TextField from "@material-ui/core/TextField";
+import AddIcon from "@material-ui/icons/Add";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { db } from "./firebaseConfig";
+import firebase from "firebase";
 
+import ToDoList from "./components/TodoList";
 
-import ToDoList from './components/TodoList'; 
-
-const App =  () => { 
-    const useStyles = makeStyles((theme) => ({
+const App = () => {
+  const useStyles = makeStyles((theme) => ({
     button: {
-        margin: theme.spacing(1),
+      margin: theme.spacing(1),
     },
-    }));
+  }));
 
-    const classes = useStyles();
- 
+  const classes = useStyles();
 
-    const [toDoInput , setToDOInput] = useState('');
+  const [toDoInput, setToDOInput] = useState("");
 
-    const [toDoList , setToDoList] = useState([]);
- 
-    useEffect(() => {
-        fetchToDoList();
-    }, [])
- 
+  const [toDoList, setToDoList] = useState([]);
 
-    const toDoInputHandler = (e) => { 
-        setToDOInput(e.target.value); 
+  useEffect(() => {
+    fetchToDoList();
+  }, []);
+
+  const toDoInputHandler = (e) => {
+    setToDOInput(e.target.value);
+  };
+
+  const addToDoToDb = (e) => {
+    console.log(toDoInput.length);
+    if (toDoInput.length === 0) {
+      alert("Please Enter a todo");
+    } else {
+      e.preventDefault();
+      db.collection("todos").add({
+        progress: true,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        todo: toDoInput,
+      });
     }
+    setToDOInput("");
+  };
 
-    const addToDoToDb = (e) => {
-        console.log(toDoInput.length);
-        if(toDoInput.length === 0){
-            alert("Please Enter a todo");
-        }else{
-        e.preventDefault();
-        db.collection("todos").add({
-            progress : true,
-            timestamp : firebase.firestore.FieldValue.serverTimestamp(),
-            todo : toDoInput
-        });
-    }
-        setToDOInput('');
-    }
+  const fetchToDoList = () => {
+    db.collection("todos").onSnapshot((q) => {
+      setToDoList(
+        q.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          progress: doc.data().progress,
+        }))
+      );
+    });
+  };
 
-
-    const fetchToDoList = () => {
-        db.collection("todos").onSnapshot((q) => {
-            setToDoList(
-                q.docs.map((doc) => ({
-                id : doc.id,
-                todo : doc.data().todo,
-                progress : doc.data().progress,
-            }))
-            )
-        })
-    } 
- 
-
-    return(  
-        <div className = "main_container">
-            <h1>Todo List</h1>  
-            <div>
-                <form onSubmit = {addToDoToDb}>
-                    <TextField id="standard-basic" label="Write a todo" 
-                    className = "input_field" onChange = {toDoInputHandler} 
-                    value = {toDoInput}  />
-                        <Button variant="contained" color="secondary" startIcon={<AddIcon />} onClick = {addToDoToDb} className={classes.button}/> 
-                        {
-                            toDoList.map(
-                                (todo) =>
-                                <ToDoList id = {todo.id} progress = {todo.progress} todo = {todo.todo}  />
-                            )
-                        } 
-                </form> 
-            </div>          
-        </div> 
-    )
+  return (
+    <div className="main_container">
+      <h1>Todo List</h1>
+      <div>
+        <form onSubmit={addToDoToDb}>
+          <TextField
+            id="standard-basic"
+            label="Write a todo"
+            className="input_field"
+            onChange={toDoInputHandler}
+            value={toDoInput}
+          />
+          <Button
+            variant="contained"
+            color="secondary"
+            startIcon={<AddIcon />}
+            onClick={addToDoToDb}
+            className={classes.button}
+          />
+          {toDoList.map((todo) => (
+            <ToDoList id={todo.id} progress={todo.progress} todo={todo.todo} />
+          ))}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,38 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import { ListItem, ListItemText } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import { db } from '../firebaseConfig'; 
+import { ListItem, ListItemText } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import { db } from "../firebaseConfig";
 
-const ToDoList = ({id , progress , todo  }) =>{
-    const toggleProgress = () => {
-        db.collection("todos").doc(id).update({
-            progress : !progress
-        })
-    } 
- 
- 
+const ToDoList = ({ id, progress, todo }) => {
+  const toggleProgress = () => {
+    db.collection("todos").doc(id).update({
+      progress: !progress,
+    });
+  };
 
-    const deletToDO = () => {
-        db.collection("todos").doc(id).delete();
-    }
-    return( 
-        <div className = 'mainDiv'  style = {{display : 'flex' , flexDirection : 'row' , justifyContent : 'space-around' , marginTop : '10px' ,  marginBottom : '10px' , borderStyle : 'solid' , fontSize : '10px'}}>
-                     <ListItem>
-                         <ListItemText primary = {todo} 
-                          secondary = {progress ? "In Progress⌚" : "Completed✅"} />
-                     </ListItem>
-                     <Button
-                     onClick = {toggleProgress} 
-                     >
-                         {progress ? "Done" : "UnDone"}
-                     </Button>
-                     <Button onClick = {deletToDO}>
-                         X
-                     </Button>
-                      
-                      </div>
-    )
-}
+  const deletToDO = () => {
+    db.collection("todos").doc(id).delete();
+  };
+  return (
+    <div
+      className="mainDiv"
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        marginTop: "10px",
+        marginBottom: "10px",
+        borderStyle: "solid",
+        fontSize: "10px",
+      }}
+    >
+      <ListItem>
+        <ListItemText
+          primary={todo}
+          secondary={progress ? "In Progress⌚" : "Completed✅"}
+        />
+      </ListItem>
+      <Button onClick={toggleProgress}>{progress ? "Done" : "UnDone"}</Button>
+      <Button onClick={deletToDO}>X</Button>
+    </div>
+  );
+};
 
 export default ToDoList;
